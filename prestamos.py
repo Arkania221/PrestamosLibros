18d4219a4fd367d29fd6601a293ae62b483c83ce
Ángel Borrás <149078725+Arkania221@users.noreply.github.com>
2024-01-25 09:48:42 +0100

    Casi finalizado
    Me queda solucionar un error a la hora de prestar

--- prestamos.py
+++ prestamos.py
@@ -1,7 +1,7 @@
 libros = []
 libros_prestados = []
 while True:
-    print("Selecciona una opcion: Añadir libros = 1, Eliminar libros = 2, Mostrar libros = 3, Prestar libro = 4")
+    print("Selecciona una opcion: Añadir libros = 1, Eliminar libros = 2, Mostrar libros = 3, Prestar libro = 4, devolver prestamos = 5")
 
     Seleccion = int(input())
 
@@ -17,12 +17,29 @@ while True:
         else:
             print("libro no encontrado")
     elif Seleccion == 3: #Solo printea la lista de libros
-        print(libros)
-    elif Seleccion = 4:
+        for libros in libros:
+            print("Nombre:" + libros)
+        for libros_prestados in libros_prestados:
+            print("Prestamo" + libros_prestados)
+    elif Seleccion == 4: #Primero recoge el valor persona, recoge el valor libro, si el libro existe mezcla los dos valores mas ":" y borra el libro de la lista libros
         print("a quien va prestado?")
         persona = input()
         print("Que libro va a ser prestado?")
-        if libro in
+        libro = input()
+        if libro in libros:
+            libros_prestados.append(persona + ":" + libro)
+            libros.remove(libro)
+    elif Seleccion == 5: #Realiza la misma tarea pero a la inversa, agrega el libro en la lista libros y borra el prestamo realizaddo con anteiroridad           libro
+        print("A quien fue prestado?")
+        persona = input()
+        print("Que libro fue prestado?")
+        libro = input()
+        libro_prestado = (persona + ":" + libro)
+        if libro_prestado in libros_prestados:
+            libros_prestados.remove(persona + ":" + libro)
+            libros.append(libro)
+        else:
+            print("no esta")
     else:
         print("Opcion invalida")
         
