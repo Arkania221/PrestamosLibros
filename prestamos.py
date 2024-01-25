09228c660cf4284a43ec16657b590e3c95a39e0b
Ángel Borrás <149078725+Arkania221@users.noreply.github.com>
2024-01-25 09:21:12 +0100

    Primer commit
    no pude hacerlo desde git, asi que arrastre y solte el archivop

---
+++ prestamos.py
@@ -0,0 +1,23 @@
+libros = []
+
+while True:
+    print("Selecciona una opcion: Añadir libros = 1, Eliminar libros = 2, Mostrar libros = 3")
+
+    Seleccion = int(input())
+
+    if Seleccion == 1: #En esta seleccion se agrega un libro pidiendo al user que agregue un valor, este sera tomado como str y se agregara a la lista con un .append
+        print("Escribe el nombre del libro")
+        libro = str(input())
+        libros.append(libro)
+    elif Seleccion == 2: #Tiene el mismo funcionamiento que la primera parte pero cambiando el .append por un .remove y agregando una comprobacion
+        print("Escribe el nombre del libro")
+        libro = input()
+        if libro in libros:
+            libros.remove(libro)
+        else:
+            print("libro no encontrado")
+    elif Seleccion == 3: #Solo printea la lista de libros
+        print(libros)
+    else:
+        print("Opcion invalida")
+        
